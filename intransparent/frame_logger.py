d0b572e51e40ffecc5a54adf356a187a603411ce
Robert Grimm <apparebit@gmail.com>
2023-05-29 01:09:56 -0400

    Define FrameLogger protocol base on display's show_info/show_table

---
+++ intransparent/frame_logger.py
@@ -0,0 +1,26 @@
+from typing import Protocol
+import pandas as pd
+
+
+class FrameLogger(Protocol):
+    """
+    A frame logger is a function that prints information about a dataframe,
+    which is passed as its only positional argument. Optional named arguments
+    provide a title and/or description.
+    """
+    def __call__(
+        self,
+        frame: pd.DataFrame,
+        *,
+        title: None | str = None,
+        description: None | str = None,
+    ) -> None: ...
+
+
+def silent_logger(
+    frame: pd.DataFrame,
+    *,
+    title: None | str = None,
+    description: None | str = None
+) -> None:
+    pass
