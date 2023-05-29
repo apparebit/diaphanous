f84248bb60c66fac619602a8124d35106e5143d1
Robert Grimm <apparebit@gmail.com>
2023-05-29 01:11:29 -0400

    Account for API changes

--- intransparent/__init__.py
+++ intransparent/__init__.py
@@ -1,13 +1,15 @@
 # Handle Dataset 1: CSAM Reports per Platform
 from .platform.data import REPORTS_PER_PLATFORM
 from .platform.ingest import ingest_reports_per_platform
-from .platform.export import export_reports_per_platform
+from .platform.export import dumps_reports_per_platform
 from .platform.compare import compare_all_platform_reports
 
 # Handle Dataset 2: CSAM Reports per Country
 from .country import (
+    YEAR_LABELS,
     ingest_reports_per_country,
     without_populations,
+    reports_per_capita_country_year,
 )
 
 # Shared utilities for nicely rendering tables
