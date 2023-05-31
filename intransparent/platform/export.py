9505620a5459a6a9adfe6992581c21273098e07a
Robert Grimm <apparebit@gmail.com>
2023-05-31 14:31:10 -0400

    Rewrite JSON encoding using generators instead of line buffers

--- intransparent/platform/export.py
+++ intransparent/platform/export.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 import json
 from typing import cast
 
@@ -35,67 +36,81 @@ def _format_row_json(row: RowType) -> str:
     return line
 
 
-def _format_citation(metadata: MetadataType):
+def _format_citation(metadata: MetadataType) -> Iterator[str]:
     keys, values = zip(*metadata.items())
     key_width = max(len(key) for key in keys)
     rule_width = key_width + max(len(value) for value in values) + 10
     rule = "━" * rule_width
 
     # In ASCII and UTF-8: '!' < [A-Za-z] < '|'
-    lines = [f'    "!": "{rule}",']
+    yield f'    "!": "{rule}",'
     for key, value in zip(keys, values):
         key = json.dumps(key).rjust(key_width + 2 + 4)
-        lines.append(f"       {key}: {json.dumps(value)},")
-    lines.append(f'    "|": "{rule}"')
-    lines.append("  }")
-
-    return lines
+        yield f"       {key}: {json.dumps(value)},"
+    yield f'    "|": "{rule}"'
+    yield "  }"
 
 
-def dumps_reports_per_platform(platform_disclosures: DisclosureCollectionType) -> str:
-    lines = ["{"]
+def encode_reports_per_platform(
+    platform_disclosures: DisclosureCollectionType
+) -> Iterator[str]:
+    previous_line: None | str = None
 
-    def append_comma_if_not(flag: bool) -> bool:
+    def emit_line(line: str) -> Iterator[str]:
+        nonlocal previous_line
+
+        if previous_line is not None:
+            yield previous_line
+        previous_line = line
+
+    def append_comma_to_line_if_not(flag: bool) -> bool:
+        nonlocal previous_line
+
         if not flag:
-            lines[-1] += ","
+            assert previous_line is not None
+            previous_line += ","
         return False
+
+    yield from emit_line("{")
 
     first_platform = True
     for platform, platform_object in platform_disclosures.items():
-        first_platform = append_comma_if_not(first_platform)
+        first_platform = append_comma_to_line_if_not(first_platform)
 
         if platform_object is None:
-            lines.append(f"  {json.dumps(platform)}: null")
+            yield from emit_line(f"  {json.dumps(platform)}: null")
             continue
 
-        lines.append(f"  {json.dumps(platform)}: {{")
+        yield from emit_line(f"  {json.dumps(platform)}: {{")
         if platform == "@":
-            lines.extend(_format_citation(cast(MetadataType, platform_object)))
+            for line in _format_citation(cast(MetadataType, platform_object)):
+                yield from emit_line(line)
             continue
 
         first_property = True
         for key, value in cast(DisclosureType, platform_object).items():
-            first_property = append_comma_if_not(first_property)
+            first_property = append_comma_to_line_if_not(first_property)
 
             if key == "brands":
                 s = ", ".join([json.dumps(item) for item in cast(list[str], value)])
-                lines.append(f'    "brands": [{s}]')
+                yield from emit_line(f'    "brands": [{s}]')
             elif key == "row_index":
-                lines.append(f'    "row_index": {json.dumps(value)}')
+                yield from emit_line(f'    "row_index": {json.dumps(value)}')
             elif key in ("columns", "comments", "rows", "nonintegers", "sources"):
-                lines.append(f'    "{key}": [')
+                yield from emit_line(f'    "{key}": [')
                 first_item = True
                 for item in cast(list, value):
-                    first_item = append_comma_if_not(first_item)
+                    first_item = append_comma_to_line_if_not(first_item)
                     if key == "rows":
-                        lines.append(_format_row_json(item))
+                        yield from emit_line(_format_row_json(item))
                     else:
-                        lines.append(f"      {json.dumps(item)}")
-                lines.append(f"    ]")
+                        yield from emit_line(f"      {json.dumps(item)}")
+                yield from emit_line(f"    ]")
             else:
                 raise ValueError(f'Unknown platform object property "{key}"')
 
-        lines.append("  }")
+        yield from emit_line("  }")
 
-    lines.append("}")
-    return "\n".join(lines)
+    yield from emit_line("}")
+    assert previous_line is not None
+    yield previous_line
