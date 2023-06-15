d8db239f43207ddec038e438834c2b3b83ddecc7
Robert Grimm <apparebit@gmail.com>
2023-06-15 15:20:10 -0400

    Improve choropleth generation
    * Country boundaries now use thinner lines, so they convey more detail.
    * The reconstruction of which year has which geo trace now uses two heuristics and hence is more robust

--- intransparent/main.py
+++ intransparent/main.py
@@ -6,6 +6,7 @@ import sys
 import traceback
 from typing import Any
 
+import numpy as np
 import pandas as pd
 import plotly.express as px  # type: ignore[import]
 
@@ -30,11 +31,10 @@ def choropleth(frame: pd.DataFrame, with_panels: bool, with_antarctica: bool) ->
         color='reports_per_capita',
         color_continuous_scale=px.colors.sequential.Plasma_r,
         range_color=(0, 0.042), # Extra 0.002 helps offset ticks from label
-        hover_name='labels',
-        hover_data={'iso3': False, 'reports_per_capita': False, 'year': False},
+        #hover_name='labels',
+        #hover_data={'iso3': False, 'reports_per_capita': False, 'year': False},
         labels={'reports_per_capita': 'Reports<br>per Capita'},
     )
-
     if with_panels:
         kwargs = kwargs | dict(
             facet_row='year',
@@ -47,12 +47,24 @@ def choropleth(frame: pd.DataFrame, with_panels: bool, with_antarctica: bool) ->
 
     fig = px.choropleth(frame, **kwargs)
 
+    # Also see "countrywidth" under update_geos() below
+    fig.update_traces(
+        marker_line_width=0.4,
+        selector=dict(
+            type='choropleth',
+        ),
+    )
+
     fig.update_geos(
-        resolution=110,
+        resolution=110, # 50 is too much detail
         showframe=False,
+        showlakes=False,
         showocean=True,
         oceancolor='#eaeaec',
-        showlakes=False,
+        showcoastlines=True,
+        coastlinewidth=0.4,
+        # Also see "marker_line_width" under update_traces() above
+        countrywidth=0.4,
     )
     if not with_antarctica:
         fig.update_geos(lataxis_range=[-58, 90])
@@ -84,8 +96,29 @@ def choropleth(frame: pd.DataFrame, with_panels: bool, with_antarctica: bool) ->
         )
 
     if with_panels:
-        # Determine the effective mapping from year to geo axis.
-        trace_by_year = {d.customdata[0][2] : d.geo for d in fig.data}
+        # Heuristically determine the effective mapping from year to geo axis.
+        trace_by_year = {}
+        for obj in fig.data:
+            # It takes just one option, facet_row, to produce four choropleths.
+            # But sadly, Plotly just won't tell me what's what.
+            year = None
+            trace = obj.geo
+
+            # Heuristic: When created WITH hover_name and hover_data.
+            if isinstance(obj.customdata, np.ndarray):
+                year = obj.customdata[0][2]
+
+            # Heuristic: When created WITHOUT hover_name and hover_data
+            elif isinstance(obj.hovertemplate, str):
+                if obj.hovertemplate.startswith('year=20'):
+                    year = obj.hovertemplate[5:9]
+
+            # We really need that mapping!
+            if year is None:
+                raise ValueError(
+                    f'could not match {trace} with year, need another heuristic')
+
+            trace_by_year[year] = trace
 
         def format_annotation(a: Any) -> None:
             # For facet rows, Plotly places titles on right side, rotated by 90º.
