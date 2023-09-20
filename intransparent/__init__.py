43b9361c8f41fffb5a9e2f1ced12cc72b747348c
Robert Grimm <apparebit@gmail.com>
2023-09-20 13:11:32 -0400

    add quasi-redundant __all__

--- intransparent/__init__.py
+++ intransparent/__init__.py
@@ -1,3 +1,20 @@
+# You are not drunk. Python is.
+__all__ = (
+    'REPORTS_PER_PLATFORM',
+    'ingest_reports_per_platform',
+    'encode_reports_per_platform',
+    'compare_all_platform_reports',
+    'YEAR_LABELS',
+    'ingest_reports_per_country',
+    'without_populations',
+    'reports_per_capita_country_year',
+    'create_map',
+    'DisplayMethod',
+    'show_map',
+    'show',
+    'to_schema',
+)
+
 # Handle Dataset 1: CSAM Reports per Platform
 from .platform.data import REPORTS_PER_PLATFORM
 from .platform.ingest import ingest_reports_per_platform
@@ -14,4 +31,5 @@ from .country import (
 
 from .mapping import create_map, DisplayMethod, show_map
 
+# Help display things
 from .show import show, to_schema
