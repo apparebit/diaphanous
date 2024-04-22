9f6c874cb8173919e3f62531363c9ea8bb384e49
Robert Grimm <apparebit@gmail.com>
2024-04-21 21:36:38 -0400

    complete analysis of TikTok data

--- intransparent/tiktok.py
+++ intransparent/tiktok.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 import re
-from typing import Self
+from typing import Literal, Self
 
 import pandas as pd
 
@@ -32,22 +32,24 @@ class Processor:
 
     year: int
     quarter: int
+    status: Literal["all", "quarter", "shares"]
     data: pd.DataFrame
 
     @classmethod
-    def for_quarter(cls, period: str | pd.Period, directory: None | Path = None) -> Self:
+    def for_period(cls, period: str | pd.Period, directory: None | Path = None) -> Self:
         year, quarter = parse_quarter(period)
         if directory is None:
             directory = Path(__file__).parent.parent / "data" / "tiktok"
         path = directory / f"tiktok-{year}-q{quarter}.csv"
-        data = pd.read_csv_directory(path)
-        return cls(year, quarter, data)
+        data = pd.read_csv(path)
+        return cls(year, quarter, "all", data)
 
-    def focus(self) -> Self:
-        return type(self)(self.year, self.quarter, self.data[
-            self.data["Period"] == f"{_QUARTER_LABELS[self.quarter-1]} {self.year}"
-            & self.data["Market"] == "All"
-            & self.data["Task"] == "All"
+    def quarter_only(self) -> Self:
+        assert self.status == "all"
+        return type(self)(self.year, self.quarter, "quarter", self.data[
+            (self.data["Period"] == f"{_QUARTER_LABELS[self.quarter-1]} {self.year}")
+            & (self.data["Market"] == "All")
+            & (self.data["Task"] == "All")
         ])
 
     def total_videos_removed(self) -> int:
@@ -55,9 +57,60 @@ class Processor:
         Retrieve the count of total videos removed. This processor must be
         `focus()`ed before invoking this method.
         """
+        assert self.status == "quarter"
         result = self.data.loc[self.data["Metric"] == "Total videos removed", "Result"]
         assert len(result) == 1
         return int(result.iat[0])
 
-    def category_share(self) -> pd.DataFrame:
-        return self.data.loc[self.data["Metric"] == "Category share"]
+    def category_shares(self) -> Self:
+        assert self.status == "quarter"
+        return type(self)(self.year, self.quarter, "shares", self.data[
+            (self.data["Metric"] == "Category share")
+            & (self.data["Issue policy"].str.contains("Youth")
+               | (self.data["Issue policy"] == "Safety & Civility"))
+        ])
+
+    def stats(self) -> pd.DataFrame:
+        assert self.status == "quarter"
+        total_count = self.total_videos_removed()
+
+        shares = self.category_shares()
+        category_share = (
+            shares.data
+            .loc[self.data["Issue policy"] == "Safety & Civility", "Result"]
+            .iat[0]
+        )
+        subcategory_share = (
+            shares.data
+            .loc[self.data["Issue policy"] == "Youth Exploitation & Abuse", "Result"]
+            .iat[0]
+        )
+        subcategory_count = int(total_count * category_share * subcategory_share)
+        alt_subcategory_share = (
+            shares.data
+            .loc[self.data[
+                "Issue policy"] == "Youth Safety & Well-Being - Youth Exploitation & Abuse",
+                "Result"
+            ]
+            .iat[0]
+        )
+
+        return pd.DataFrame(
+            [
+                total_count,
+                category_share,
+                subcategory_share,
+                subcategory_count,
+                alt_subcategory_share,
+                int(subcategory_count * alt_subcategory_share),
+            ],
+            columns=["Statistics"],
+            index=[
+                "Total videos removed",
+                "Category share: Safety & civility",
+                "Subcategory share: Youth exploitation & abuse",
+                "Subcategory count: Youth exploitation & abuse",
+                "Subcategory share: Youth safety & well-being - youth exploitation & abuse",
+                "Speculative subcategory count: Previous two rows multiplied"
+            ]
+        )
