86e1d0f6e755e60bcf530a903214b54fd0f8373d
Robert Grimm <apparebit@gmail.com>
2024-04-28 14:12:27 -0400

    fix export of platform breakdown data and snapshot fixed data

--- intransparent/platform/export.py
+++ intransparent/platform/export.py
@@ -11,7 +11,7 @@ from .type import (
 )
 
 
-def _format_row_json(row: RowType) -> str:
+def _format_row_json(row: RowType, indent: str) -> str:
     for header, cell_values in row.items():
         if header != "redundant":
             break
@@ -23,14 +23,14 @@ def _format_row_json(row: RowType) -> str:
         elif isinstance(cell, int):
             cells.append(f"{cell}")
         elif isinstance(cell, float):
-            cells.append(f"{cell:f}")
+            cells.append(f"{cell:.3f}")  # No quantity has more significant digits
         elif isinstance(cell, str):
             cells.append(f'"{cell}"')
         else:
             raise ValueError(f'Invalid cell "{cell}"')
 
     line = ", ".join(cells)
-    line = f"      {{{json.dumps(header)}: [{line}]"
+    line = f"{indent}{{{json.dumps(header)}: [{line}]"
     if row.get("redundant"):
         line = f'{line}, "redundant": true'
     line = f"{line}}}"
@@ -44,12 +44,12 @@ def _format_citation(metadata: MetadataType) -> Iterator[str]:
     rule = "━" * rule_width
 
     # In ASCII and UTF-8: '!' < [A-Za-z] < '|'
-    yield f'    "!": "{rule}",'
+    yield f'        "!": "{rule}",'
     for key, value in zip(keys, values):
         key = json.dumps(key).rjust(key_width + 2 + 4)
-        yield f"       {key}: {json.dumps(value)},"
-    yield f'    "|": "{rule}"'
-    yield "  }"
+        yield f"           {key}: {json.dumps(value)},"
+    yield f'        "|": "{rule}"'
+    yield "    }"
 
 
 def encode_reports_per_platform(
@@ -72,6 +72,21 @@ def encode_reports_per_platform(
             previous_line += ","
         return False
 
+    def emit_list(key: str, values: list[str | RowType], indent: str) -> Iterator[str]:
+        if len(values) == 1:
+            yield from emit_line(f'{indent}"{key}": [{json.dumps(values[0])}]')
+            return
+
+        yield from emit_line(f'{indent}"{key}": [')
+        first_item = True
+        for item in values:
+            first_item = append_comma_to_line_if_not(first_item)
+            if isinstance(item, str):
+                yield from emit_line(f"{indent}    {json.dumps(item)}")
+            else:
+                yield from emit_line(_format_row_json(item, indent + "    "))
+        yield from emit_line(f"{indent}]")
+
     yield from emit_line("{")
 
     first_platform = True
@@ -79,10 +94,10 @@ def encode_reports_per_platform(
         first_platform = append_comma_to_line_if_not(first_platform)
 
         if platform_object is None:
-            yield from emit_line(f"  {json.dumps(platform)}: null")
+            yield from emit_line(f"    {json.dumps(platform)}: null")
             continue
 
-        yield from emit_line(f"  {json.dumps(platform)}: {{")
+        yield from emit_line(f"    {json.dumps(platform)}: {{")
         if platform == "@":
             for line in _format_citation(cast(MetadataType, platform_object)):
                 yield from emit_line(line)
@@ -94,35 +109,25 @@ def encode_reports_per_platform(
 
             if key == "brands":
                 s = ", ".join([json.dumps(item) for item in cast(list[str], value)])
-                yield from emit_line(f'    "brands": [{s}]')
-            elif key in ("features", "schema"):
-                yield from emit_line(f'    "{key}": {{')
+                yield from emit_line(f'            "brands": [{s}]')
+            elif key in ("features", "products", "schema", "sums"):
+                yield from emit_line(f'        "{key}": {{')
                 first_item = True
                 for k, v in cast(dict, value).items():
                     first_item = append_comma_to_line_if_not(first_item)
                     if v is None:
-                        s = "null"
+                        yield from emit_line(f'            "{k}": null')
                     elif isinstance(v, str):
-                        s = f'"{v}"'
+                        yield from emit_line(f'            "{k}": {json.dumps(v)}')
                     else:
-                        s = ", ".join(f'"{el}"' for el in v)
-                        s = f'[{s}]'
-                    yield from emit_line(f'        "{k}": {s}')
-                yield from emit_line('    }')
-            elif key in ("aggregates", "columns", "comments", "rows", "sources"):
-                yield from emit_line(f'    "{key}": [')
-                first_item = True
-                for item in cast(list, value):
-                    first_item = append_comma_to_line_if_not(first_item)
-                    if key == "rows":
-                        yield from emit_line(_format_row_json(item))
-                    else:
-                        yield from emit_line(f"      {json.dumps(item)}")
-                yield from emit_line(f"    ]")
+                        yield from emit_list(k, v, "            ")
+                yield from emit_line('        }')
+            elif key in ("columns", "comments", "rows", "sources"):
+                yield from emit_list(key, value, "        ")
             else:
                 raise ValueError(f'Unknown platform object property "{key}"')
 
-        yield from emit_line("  }")
+        yield from emit_line("    }")
 
     yield from emit_line("}")
     assert previous_line is not None
