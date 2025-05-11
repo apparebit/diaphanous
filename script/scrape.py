56053db0b239a0591e13f0f2acadcf13ba47cc7a
Robert Grimm <apparebit@gmail.com>
2025-05-11 11:26:40 -0400

    add script for scraping DataReportal InDesign slide decks from Adobe's website

---
+++ script/scrape.py
@@ -0,0 +1,77 @@
+#!.venv/bin/python
+
+from collections.abc import Iterable
+#from urllib.parse import parse_qs, urlparse
+from playwright.sync_api import sync_playwright, Page
+
+#     # Determine deck size
+#     deck_size = page.evaluate(
+#         'JSON.parse(readerViewDataFromServer.MANIFEST_BODY).pages.length'
+#     )
+#     # Determine base URL for slides
+#     link = page.locator('iframe[name=targetFrame]').get_attribute('src')
+#     query = parse_qs(urlparse(link).query)
+#     base, _, _ = (query["basepath"][0] + query["relativepath"][0]).rpartition("/")
+#     print(f'  ⦿ base="{base}"')
+
+
+def digital2023():
+    for index in range(1, 286):
+        yield (
+            "https://indd.adobe.com/content/2/"
+            "672a3366-328d-4a51-98a5-95c1082618d2/1674650530560/package/1/"
+            f"publication-{index}.html"
+        )
+
+
+def digital2024():
+    for index in range(1, 289):
+        yield (
+            "https://indd.adobe.com/content/2/"
+            "eede3550-b3a7-4ebf-b417-3258cb4f15dc/3416970665860/package/82zp/"
+            f"publication-{index}.html"
+        )
+
+
+def capture_slide(page: Page, url: str, path: str) -> None:
+    print(f'capture "{url}"')
+    page.goto(url)
+    page.pdf(
+        path=path,
+        width="1080px",
+        height="1920px",
+        landscape=True,
+        print_background=True,
+    )
+
+def capture_deck(name: str, urls: Iterable[str]) -> None:
+    with sync_playwright() as playwright:
+        # Scrape in the open: Use Chrome and the correct user agent.
+        # Also, no stealth measures.
+        browser = playwright.chromium.launch(
+            channel="chrome",
+            headless=False,
+        )
+
+        context = browser.new_context(
+            user_agent=(
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 '
+                '(KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
+            ),
+            viewport={"width": 1920, "height": 1080},
+            device_scale_factor=2,
+            is_mobile=False,
+        )
+
+        page = context.new_page()
+
+        try:
+            for index, url in enumerate(urls):
+                capture_slide(page, url, f"{name}/slide-{index+1:03}.pdf")
+        finally:
+            browser.close()
+
+capture_deck("digital2024", digital2024())
+capture_deck("digital2023", digital2023())
+
+# When done: pdfunite slide-*.pdf digital202x.pdf
