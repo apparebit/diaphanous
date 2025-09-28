0b5a168949a9baa6f10518f66313ebbb0e0f7e6d
Robert Grimm <apparebit@gmail.com>
2025-09-28 05:50:30 -0400

    move output hook for R from notebook into package

--- diaphanous/arr.py
+++ diaphanous/arr.py
@@ -14,9 +14,11 @@ R ("arr") integration:
     ```
 """
 from collections.abc import Sequence
+from io import StringIO
 from pathlib import Path
 
-import rpy2.robjects.packages as r_packages
+import rpy2.rinterface_lib as rinterface_lib
+import rpy2.robjects.packages as rpackages
 from rpy2.robjects.vectors import StrVector
 
 
@@ -63,7 +65,7 @@ def install(
     Path(rlib).mkdir(exist_ok=True)
 
     # Check for uninstalled packages
-    todo = [p for p in packages if not r_packages.isinstalled(p, lib_loc=rlib)]
+    todo = [p for p in packages if not rpackages.isinstalled(p, lib_loc=rlib)]
     if not todo:
         return
 
@@ -77,6 +79,52 @@ def install(
     msg(s)
 
     # Install missing R packages
-    r_utils = r_packages.importr("utils")
+    r_utils = rpackages.importr("utils")
     r_utils.chooseCRANmirror(ind=1)
     r_utils.install_packages(StrVector(todo), lib=rlib)
+
+
+if not IS_NOTEBOOK:
+
+    def enable_write_hook() -> None:
+        pass
+
+    def disable_write_hook() -> None:
+        pass
+
+else:
+
+    _write_console_buffer = StringIO()
+    _original_consolewrite_print = rinterface_lib.callbacks.consolewrite_print
+
+    def flush_console() -> None:
+        from IPython.display import display, HTML
+
+        if _write_console_buffer.tell() == 0:
+            return
+
+        s = _write_console_buffer.getvalue()
+        display(HTML(f"<pre><code>{s}</code></pre>"))
+        _write_console_buffer.seek(0)
+        _write_console_buffer.truncate(0)
+
+    def write_console(s: str) -> None:
+        from IPython.display import display, HTML
+
+        if s == "[1]":
+            return
+
+        if s.startswith(' "') and s.endswith('"'):
+            s = s[2:-1]
+        if not s.startswith("𝐇"):
+            _write_console_buffer.write(s)
+            return
+
+        flush_console()
+        display(HTML(s[1:]))
+
+    def enable_write_hook() -> None:
+        rinterface_lib.callbacks.consolewrite_print = write_console
+
+    def disable_write_hook() -> None:
+        rinterface_lib.callbacks.consolewrite_print = _original_consolewrite_print
