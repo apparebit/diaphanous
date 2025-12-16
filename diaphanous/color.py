970a29e98e43c27244da4ab1baafcda3a37d4f36
Robert Grimm <apparebit@gmail.com>
2025-12-16 21:26:40 +0100

    add pink color scale and update blue one, both based on Oklch

--- diaphanous/color.py
+++ diaphanous/color.py
@@ -32,6 +32,19 @@ _COLORS = [c for c in Palette.__members__.values() if c is not Palette.GRAY]
 
 
 class Scale(enum.Enum):
+    PINK = (
+        "#ff8ab7",
+        "#ef7ca9",
+        "#df6e9b",
+        "#d0608d",
+        "#c05280",
+        "#b14472",
+        "#a23665",
+        "#932759",
+        "#84174c",
+        "#750040",
+    )
+
     RED = (
         "#ffb950",
         "#ffad33",
@@ -46,15 +59,58 @@ class Scale(enum.Enum):
     )
 
     BLUE = (
-        #"#caf0f8",
-        "#ade8f4",
-        "#90e0ef",
-        "#48cae4",
-        "#00b4d8",
-        "#0096c7",
-        "#0077b6",
-        "#015ba0",
-        "#023e8a",
-        "#032174",
-        "#03045e",
+        "#97bbf5",
+        "#84a7e0",
+        "#7294cc",
+        "#6081b7",
+        "#4e6fa4",
+        "#3d5d90",
+        "#2c4b7d",
+        "#1c3a6a",
+        "#0c2958",
+        "#001946",
     )
+
+    # BLUE = (
+    #     #"#caf0f8",
+    #     "#ade8f4",
+    #     "#90e0ef",
+    #     "#48cae4",
+    #     "#00b4d8",
+    #     "#0096c7",
+    #     "#0077b6",
+    #     "#015ba0",
+    #     "#023e8a",
+    #     "#032174",
+    #     "#03045e",
+    # )
+
+
+def vary_lightness(min: float, max: float, steps: int, c: float, h: float) -> list[str]:
+    from prettypretty.color import Color
+
+    colors = []
+    delta = (max - min) / (steps - 1)
+
+    l = max
+    for _ in range(steps):
+        colors.append(Color.oklch(l, c, h).to_hex_format())
+        l -= delta
+
+    return colors
+
+
+if __name__ == "__main__":
+    from prettypretty.color import Color, ColorSpace
+
+    print("    PINK = (")
+    pink = Color.parse(Palette.PINK).to(ColorSpace.Oklch)
+    for color in vary_lightness(0.366, pink[0], 10, pink[1], pink[2]):
+        print(f'        "{color}",')
+    print("    )\n")
+
+    print("    BLUE = (")
+    blue = Color.parse(Palette.LIGHT_BLUE).to(ColorSpace.Oklch)
+    for color in vary_lightness(0.2285, blue[0], 10, blue[1], blue[2]):
+        print(f'        "{color}",')
+    print("    )")
